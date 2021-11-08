80935de80521601972f94d4fe3eabd26c8221023
Akhil-EM <akhilemcalicut@gmail.com>
2021-11-08 15:55:44 +0530

    how the database can be access and print results to the view

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -3,6 +3,7 @@
 
      public function index(){
        $this -> load -> model("customer_model");
+      //  data object
        $data["users"] = $this -> customer_model -> get_users();
        $this-> load-> view("customer-view",$data);
        
